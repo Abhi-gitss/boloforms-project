2eabdec235f0659581aa9f3fd9c5c9306e289d8f
Abhijeet <ak8742068@gmail.com>
2025-12-12 11:33:34 +0530

    Update CORS configuration in backend

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+const FRONTEND_URL = process.env.FRONTEND_URL || 'https://boloforms-project.vercel.app/';
+app.use(cors({ origin: FRONTEND_URL }));
 app.use(express.json({ limit: '50mb' }));
 
 // Mongo connection string must be in env var for safety.
